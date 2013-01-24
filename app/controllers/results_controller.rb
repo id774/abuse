01be669bcafdefe76c7055bed5c88e92eb641a5a
id774 <idnanashi@gmail.com>
2013-01-24 09:09:51 +0900

    19 Move BootstrapPaginationRenderer to ApplicationController (for DRY)

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -42,24 +42,3 @@ class ResultsController < ApplicationController
     nouns
   end
 end
-
-class BootstrapPaginationRenderer < WillPaginate::ActionView::LinkRenderer
-  private
-  def previous_or_next_page(page, text, classname)
-    link(text, page, :class => classname) unless page == false
-  end
-
-  public
-  def to_html
-    html = pagination.map do |item|
-      tag(:li,
-        ((item.is_a?(Fixnum))?
-          page_number(item) :
-          send(item)))
-    end.join(@options[:link_separator])
-
-    html = tag(:ul, html)
-
-    @options[:container] ? html_container(html) : html
-  end
-end
--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -41,24 +41,3 @@ class StatusesController < ApplicationController
     end
   end
 end
-
-class BootstrapPaginationRenderer < WillPaginate::ActionView::LinkRenderer
-  private
-  def previous_or_next_page(page, text, classname)
-    link(text, page, :class => classname) unless page == false
-  end
-
-  public
-  def to_html
-    html = pagination.map do |item|
-      tag(:li,
-        ((item.is_a?(Fixnum))?
-          page_number(item) :
-          send(item)))
-    end.join(@options[:link_separator])
-
-    html = tag(:ul, html)
-
-    @options[:container] ? html_container(html) : html
-  end
-end
